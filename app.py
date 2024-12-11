4c29978aa82e4189fd87f0af6cc73819d50189f4
Ruslan Work <avangard@mail.ru>
2024-12-11 16:37:45 +0300

    приложение изменено, добавлена авторизация

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, request, Response
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+# Моковые данные пользователя
+users = {
+    "admin": "password123"  # Логин: admin, Пароль: password123
+}
+
+# Проверка пользователя и пароля
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and users[username] == password:
+        return username
+    return None
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@auth.login_required  # Требуется авторизация
+def index():
+    return "Добро пожаловать на защищённую страницу!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True) 
+    app.run(debug=True, host="0.0.0.0", port=5000)
